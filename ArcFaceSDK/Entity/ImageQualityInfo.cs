8011a4301584935cdc57e2d43bd13161e9d5b08c
SkyStonePan <phb8195@arcsoft.com.cn>
2020-06-22 13:50:42 +0800

    源码上传

---
+++ ArcFaceSDK/Entity/ImageQualityInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace ArcFaceSDK.Entity
+{
+    /// <summary>
+    /// 图像质量检测结构体
+    /// </summary>
+    public class ImageQualityInfo
+    {
+        ///<summary>
+        ///人脸质量结果集
+        /// </summary>
+        public float[] faceQualityValues { get; set; }
+
+        ///<summary>
+        ///人脸数量
+        /// </summary>
+        public int num { get; set; }
+    }
+}
---
+++ ArcFaceSDK/SDKModels/ASF_ImageQualityInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArcFaceSDK.SDKModels
+{
+    /// <summary>
+    /// 图像质量检测结构体
+    /// </summary>
+    public struct ASF_ImageQualityInfo
+    {
+        ///<summary>
+        ///人脸质量结果集
+        /// </summary>
+        public IntPtr faceQualityValues;
+
+        ///<summary>
+        ///人脸数量
+        /// </summary>
+        public int num;
+    }
+}
